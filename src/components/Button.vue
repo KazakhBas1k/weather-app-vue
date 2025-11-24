033876d80262ad7091fc53549249373b86ed317a
Balmukhambet Darkhan <darhan.balmukhambet@gmail.com>
2025-11-24 16:30:56 +0500

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button">Button</button>
+</template>
+
+<style scoped>
+.button {
+  border: none;
+  border-radius: 10px;
+  background: var(--gradient);
+  color: var(--primary);
+  padding: 10px;
+  font-weight: 600;
+  font-size: 20px;
+}
+.button:hover {
+  background: var(--gradient-inverted);
+}
+</style>
